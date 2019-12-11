c2d3cf4a9fa3219e6260590ab3e293103fe7534b
Umurzak <43066415+Umurzak@users.noreply.github.com>
2019-12-11 13:15:45 +0600

    Add files via upload

--- essayly.py
+++ essayly.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,9 +7,19 @@ def index():
     now= datetime.datetime.now()
     return render_template("main code essayly.html", now=now)
 
-@app.route("/<string:name>")
-def hello(name):
-    return f"Hello, {name}!"
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    wtype = request.form.get("wtype")
+    gender = request.form.get("gender")
+    if gender == 'Male':
+        gen = "Mr."
+    else:
+        gen = "Mrs."
+    if name == 'Aidar':
+        gen = ""
+        name = "Sir"
+    return render_template("hello.html", name=name, wtype = wtype, gen=gen)
     
 if __name__ == "__main__":
     app.run()
